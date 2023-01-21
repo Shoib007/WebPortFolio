1d11a19521faf8d7b3b9c5226a82b7af0dd1bbb9
Shoib007 <skhantanda@gmail.com>
2023-01-21 09:06:22 +0530

    Its working state without Sweetheart and MailJs

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [ 
+  {
+    title: "Python Keylogger",
+    subtitle: "Python, Multi Threading",
+    description:
+      "I created this project while I was learning Ethical Hacking in 2021. I'm very pationate about such ideas and that motivate me to think and create it.",
+    image: "https://drive.google.com/uc?id=1Evg9uQhmqP2bx3TIfTARenp0CwvsNpQj",
+    link: "https://github.com/Shoib007/Keylogger-Python",
+  },
+  {
+    title: "Pure JS Calculator",
+    subtitle: "JavaScript, HTML and CSS",
+    description:
+      "It a fully functional basic calculator support keyboard typing. I created this application when I was learning Web Development. I was a cool project as it ment me to think criticall and logically and learned to good concepts of Class and Objects.",
+    image: "./project-2.jpg",
+    link: "https://github.com/Shoib007/JSCalculator",
+  },
+  {
+    title: "Quiz Application",
+    subtitle: "Pure JS Based Quiz Application",
+    description:
+      "This was one of my very first project based on Web Application back in 2020",
+    image: "./project-3.jpg",
+    link: "https://github.com/Shoib007/online_quiz"
+  },
+  {
+    title: "My Web Portfoli0",
+    subtitle: "React JS and Tailwind",
+    description:
+      "This is my Web Portfolio based on ReactJS and for Stylling I've used Tailwind.",
+    image: "./project-4.jpg",
+    link: "http://shoibwebportfolio.rf.gd/"
+  },
+];
+
+
+export const skills = [
+  "Python",
+  "Data Structures & Algo",
+  "Django",
+  "Ethical Hacking (msfconsole, msfvenom, etc)",
+  "JavaScript",
+  "React",
+  "Node",
+  "Material UI",
+  "DBMS (MySQL, SQL Server)",
+];
